7ca268803c1ab43e92fd7fbd1c2e490888c23a0a
atda2213 <ayethidaraung2213@gmail.com>
2025-07-25 13:30:38 +0630

    feat : user management

---
+++ DisasterManagementSystem_Services/Models/UserDtos/UserCreateDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisasterManagementSystem_Services.Models.UserDtos
+{
+    internal class UserCreateDto
+    {
+    }
+}
